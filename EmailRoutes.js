0b536be359dab2fa825dcb6aa97767a22325d303
Jean Bulatao <jeanbulatao@ymail.com>
2021-05-10 13:08:58 +0300

    Failed Email

--- EmailRoutes.js
+++ EmailRoutes.js
@@ -4,10 +4,12 @@ const Register = require('./Registration/Register');
 const Confirmation = require('./Confirmation/Confirmation');
 const Thankyou = require('./Thankyou/Thankyou');
 const woym = require('./woym/woym');
+const failed = require('./Reminder/Failed');
 
 router.post('/Register', (req, res) => Register.Register(req, res));
 router.post('/Confirm', (req, res) => Confirmation.Confirmation(req, res));
 router.post('/Thankyou',(req,res) => Thankyou.thankyou(req,res));
 router.post('/woym',(req,res) => woym.woym(req,res));
+router.post('/failed',(req,res) => failed.failed(req,res));
 
 module.exports = router;
