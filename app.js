169317c296d3ee8361b3234fcca13ad797647173
Jean Bulatao <jeanbulatao@ymail.com>
2021-08-07 02:06:26 +0300

    update

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const axios = require('axios');
 var whitelist = ['http://localhost:4200', 'https://www.el-detox.com','http://127.0.0.1:4200']
 var corsOptions = {
   origin: function (origin, callback) {
@@ -33,26 +32,7 @@ const myfatoorah = require('./MyFatoorahRoute');
 const { response } = require('express');
 app.use('/myfatoorah/', myfatoorah);
 
-app.post('/verify',(req,res) => {
-    var userkey = req.body.token
-    
-    var secret = "6LflDtwbAAAAAG2NfPnSoVhVsnx0xJVy1rYklgut"
-    // var secret = "6LeHMacbAAAAAPjhv6KTcNIaaqH1y-TJViBUvEK2"
-    axios.get('https://www.google.com/recaptcha/api/siteverify?secret='+ secret +'&response='+userkey).then(response => {
-        console.log('key data = ',response );
-        if(response.data.success){
-            return res.status(200).json({
-                response: "verification success"
-            });
-        }
-         return res.status(401).json({
-             error: "verification failed"
-         })
-    }).catch(error =>{
-        res.status(500).json({
-            error : error
-        })
-    });
-});
+const recaptcha = require('./RecaptchaRoute');
+app.use('/recaptcha/', recaptcha);
 
 app.listen(port, () => console.log('Server listening on port ' + port))
